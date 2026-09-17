cd9e50d6ea98b1fee93345716b250aeec4f34c11
jjj-135 <steamjeonguchan135@gmail.com>
2026-09-17 16:18:04 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// 구글 Apps Script 를 "웹 앱"으로 배포하고 받은 주소(https://script.google.com/macros/s/.../exec)를
+// 아래 따옴표 안에 붙여 넣는다. 비어 있으면 설문을 낼 수 없다.
+window.SURVEY_ENDPOINT = 'https://script.google.com/macros/s/AKfycbwi4kaRY9-TEJfIYHO6Dqp55H2EDvLUZkdpe-wV5HomCLu7FccIXBjPRzn2R5eBsQWe/exec';
